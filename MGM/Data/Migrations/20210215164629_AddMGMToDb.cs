59febf045189853abef6cb2186ef2a45c769304c
gabimg <gabimatei78@yahoo.com>
2021-02-16 13:09:01 +0200

    login and register

---
+++ MGM/Data/Migrations/20210215164629_AddMGMToDb.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace MGM.Data.Migrations
+{
+    public partial class AddMGMToDb : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
---
+++ MGM/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace MGM.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
